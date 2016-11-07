6ca1c1baade91ecdc86733567eb93003e0aa47bf
Nevin Daniel <nevindnl@gmail.com>
2016-11-06 21:40:36 -0800

    knapsack

--- exercises.rb
+++ exercises.rb
@@ -1,35 +1,23 @@
-class Knapsack
-  attr_accessor :weight, :value, :items
-  def initialize weight:, value:, items: []
-    @weight = weight
-    @value = value
-    @items = items
-  end
-end
-
 def knapsack total_weight, items
-  array = [Knapsack.new(weight: 0, value: 0)]
+  matrix = Array.new(items.length + 1){Array.new(total_weight + 1){0}}
 
   items.each_with_index do |item, i|
-    # possible_outcomes = array.map do |outcome|
-    #   new_outcome = Knapsack.new(
-    #     weight: outcome.weight + item.weight,
-    #     value: outcome.value + item.value,
-    #     items: outcome.items + [i]
-    #   )
-    #
-    #   new_outcome.weight < total_weight ? new_outcome : outcome
-    # end
-    # 
-    # array.push(possible_outcomes.max_by(&:value))
+    (0...total_weight).each do |j|
+      weight = item[:weight]
+      value = item[:value]
+
+      if j + 1 >= weight
+        matrix[i + 1][j + 1] = [
+          matrix[i][j + 1],
+          matrix[i + 1][j + 1 - weight] + value
+        ].max
+      else
+        matrix[i + 1][j + 1] = matrix[i][j + 1]
+      end
+    end
   end
 
-  array[-1]
-end
-
-items = []
-10.times do
-  items.push(Knapsack.new(weight: rand(10) + 1, value: rand(20 + 1)))
+  matrix[-1][-1]
 end
 
 require_relative "matrix"
